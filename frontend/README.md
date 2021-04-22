4604197197b1ce7a98775cdad1e8937adbce9431
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-04-22 15:27:53 +0200

    service worker v1.

--- frontend/README.md
+++
@@ -1,7 +0,0 @@
-### Assets
-
-
-video lakeLoop - Video by Oleg Magni from Pexels
-
- Video by Tobias Bjørkli from Pexels
-
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
@@ -19,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+serviceWorkerRegistration.register()
